8d4d341400fc653df52765478a7c0c292c3fbe2f
sceponaviciute <jone.sceponaviciute@gmail.com>
2022-09-19 22:12:20 +0300

    added forecast

--- src/app.js
+++ src/app.js
@@ -20,6 +20,42 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day}: ${hours}:${minutes}`;
 }
+function displayForecast(response) {
+  console.log(response.data);
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+
+     <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+      <img
+        src="http://openweathermap.org/img/wn/01d@2x.png"
+        alt=""
+        width="40"
+      />
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max"> 18°C </span>
+        <span class="weather-forecast-temperature-min"> 12°C </span>
+      </div>
+    </div>
+  
+  `;
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "128be8dc2da3de030d265636585ed1da";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -43,10 +79,11 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
-  let apiKey = "128be8dc2da3de030d265636585ed1da";
+  let apiKey = "21c7ec9e626f1ab56a6eb7ddd68b4b9e";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
