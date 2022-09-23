f289b76459e6f36bc6632ad12b7288a34c95de09
sceponaviciute <jone.sceponaviciute@gmail.com>
2022-09-23 23:32:50 +0300

    Concole.log removed

--- src/app.js
+++ src/app.js
@@ -73,7 +73,6 @@ function getForecast(coordinates) {
   let apiKey = "be81f193e065bf5feb2d944c7336968b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
-  console.log(apiUrl);
 }
 
 function displayTemperature(response) {
