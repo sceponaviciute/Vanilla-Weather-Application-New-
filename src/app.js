bdcf64b40fd01dbf3cec1045460a061117de4d44
sceponaviciute <jone.sceponaviciute@gmail.com>
2022-09-23 23:11:15 +0300

    Display forecast data using the API

--- src/app.js
+++ src/app.js
@@ -20,31 +20,50 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day}: ${hours}:${minutes}`;
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
 
      <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+       
       <img
-        src="http://openweathermap.org/img/wn/01d@2x.png"
+        src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
         alt=""
         width="40"
       />
       <div class="weather-forecast-temperature">
-        <span class="weather-forecast-temperature-max"> 18°C </span>
-        <span class="weather-forecast-temperature-min"> 12°C </span>
+        <span class="weather-forecast-temperature-max">${Math.round(
+          forecastDay.temp.max
+        )}°C</span>
+        <span class="weather-forecast-temperature-min">${Math.round(
+          forecastDay.temp.min
+        )}</span>
       </div>
     </div>
   
   `;
+    }
   });
 
   forecastElement.innerHTML = forecastHTML;
@@ -52,8 +71,9 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "be81f193e065bf5feb2d944c7336968b";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
+  console.log(apiUrl);
 }
 
 function displayTemperature(response) {
